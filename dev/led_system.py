2ca331bb63986543943e764202b2722dd85b20e1
Jihyun Kim <rlawlgus072@gmail.com>
2025-06-10 16:09:58 +0900

    Updated `led_system.py`
    Updated `led_system.py`

--- dev/led_system.py
+++ dev/led_system.py
@@ -1,65 +1,33 @@
-import RPi.GPIO as GPIO
-import time
+import RPi.GPIO, time as GPIO, TIME
 
-# GPIO pin setup
-RED_PIN = 21
-YELLOW_PIN = 20
-GREEN_PIN = 16
+RED, YELLOW, GREEN = 21, 20, 16
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(RED_PIN, GPIO.OUT)
-GPIO.setup(YELLOW_PIN, GPIO.OUT)
-GPIO.setup(GREEN_PIN, GPIO.OUT)
+GPIO.setup(RED, GPIO.OUT)
+GPIO.setup(YELLOW, GPIO.OUT)
+GPIO.setup(GREEN, GPIO.OUT)
 
-def turn_off_all():
-    GPIO.output([RED_PIN, YELLOW_PIN, GREEN_PIN], GPIO.LOW)
+def INIT():
+    GPIO.output([RED, YELLOW, GREEN], GPIO.LOW)
 
-def blink_led(pin, interval=0.5, times=5):
-    for _ in range(times):
-        GPIO.output(pin, GPIO.HIGH)
-        time.sleep(interval)
-        GPIO.output(pin, GPIO.LOW)
-        time.sleep(interval)
+def GO():
+    GPIO.output(GREEN, GPIO.HIGH)
 
-def blink_all_leds(interval=0.5, times=3):
-    for _ in range(times):
-        GPIO.output([RED_PIN, YELLOW_PIN, GREEN_PIN], GPIO.HIGH)
-        time.sleep(interval)
-        GPIO.output([RED_PIN, YELLOW_PIN, GREEN_PIN], GPIO.LOW)
-        time.sleep(interval)
+def STOP():
+    GPIO.output(RED, GPIO.HIGH)
 
-try:
-    print("LED Test Program (Press 'q' to quit)")
-    print("1: RED ON | 2: GREEN ON | 3: YELLOW BLINK | 4: ALL BLINK")
+def TURN():
+    for _ in range(10):
+        GPIO.output(YELLOW, GPIO.HIGH)
+        TIME.sleep(0.5)
+        GPIO.output(YELLOW, GPIO.LOW)
+        TIME.sleep(0.5)
 
-    while True:
-        key = input("Enter key (1-4 or q): ")
+def PARK():
+    for _ in range(10):
+        GPIO.output([RED, YELLOW, GREEN], GPIO.HIGH)
+        TIME.sleep(0.5)
+        GPIO.output([RED, YELLOW, GREEN], GPIO.LOW)
+        TIME.sleep(0.5)
 
-        turn_off_all()
-
-        if key == '1':
-            print("Red LED ON")
-            GPIO.output(RED_PIN, GPIO.HIGH)
-
-        elif key == '2':
-            print("Green LED ON")
-            GPIO.output(GREEN_PIN, GPIO.HIGH)
-
-        elif key == '3':
-            print("Yellow LED BLINK")
-            blink_led(YELLOW_PIN)
-
-        elif key == '4':
-            print("All LEDs BLINK")
-            blink_all_leds()
-
-        elif key.lower() == 'q':
-            print("Exiting program.")
-            break
-
-        else:
-            print("Invalid input. Try again.")
-
-finally:
     GPIO.cleanup()
-    print("GPIO cleanup completed.")
