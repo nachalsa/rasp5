47c25a91dd602ecd8075eecb51005e4a55fa4c98
Jihyun Kim <rlawlgus072@gmail.com>
2025-06-12 21:11:41 +0900

    Updated `led_system.py`
    Updated `led_system.py`

--- dev/led_system.py
+++ dev/led_system.py
@@ -1,33 +1,48 @@
-import RPi.GPIO, time as GPIO, TIME
+try:
+    import RPi.GPIO as GPIO
+    IS_RPI = True
+except (RuntimeError, ModuleNotFoundError):
+    GPIO = None
+    IS_RPI = False
+
+import time as TIME
 
 RED, YELLOW, GREEN = 21, 20, 16
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(RED, GPIO.OUT)
-GPIO.setup(YELLOW, GPIO.OUT)
-GPIO.setup(GREEN, GPIO.OUT)
+if IS_RPI:
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(RED, GPIO.OUT)
+    GPIO.setup(YELLOW, GPIO.OUT)
+    GPIO.setup(GREEN, GPIO.OUT)
+else:
+    print("WARNING: Not running on Raspberry Pi. GPIO functions disabled.")
 
 def INIT():
-    GPIO.output([RED, YELLOW, GREEN], GPIO.LOW)
+    if IS_RPI:
+        GPIO.output([RED, YELLOW, GREEN], GPIO.LOW)
 
 def GO():
-    GPIO.output(GREEN, GPIO.HIGH)
+    if IS_RPI:
+        GPIO.output(GREEN, GPIO.HIGH)
+        GPIO.output([RED, YELLOW], GPIO.LOW)
 
 def STOP():
-    GPIO.output(RED, GPIO.HIGH)
+    if IS_RPI:
+        GPIO.output(RED, GPIO.HIGH)
+        GPIO.output([GREEN, YELLOW], GPIO.LOW)
 
 def TURN():
-    for _ in range(10):
-        GPIO.output(YELLOW, GPIO.HIGH)
-        TIME.sleep(0.5)
-        GPIO.output(YELLOW, GPIO.LOW)
-        TIME.sleep(0.5)
+    if IS_RPI:
+        for _ in range(10):
+            GPIO.output(YELLOW, GPIO.HIGH)
+            TIME.sleep(0.5)
+            GPIO.output(YELLOW, GPIO.LOW)
+            TIME.sleep(0.5)
 
 def PARK():
-    for _ in range(10):
-        GPIO.output([RED, YELLOW, GREEN], GPIO.HIGH)
-        TIME.sleep(0.5)
-        GPIO.output([RED, YELLOW, GREEN], GPIO.LOW)
-        TIME.sleep(0.5)
-
-    GPIO.cleanup()
+    if IS_RPI:
+        for _ in range(10):
+            GPIO.output([RED, YELLOW, GREEN], GPIO.HIGH)
+            TIME.sleep(0.5)
+            GPIO.output([RED, YELLOW, GREEN], GPIO.LOW)
+            TIME.sleep(0.5)
