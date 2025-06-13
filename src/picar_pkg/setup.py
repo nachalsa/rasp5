a82c51a66d3426b9a851db2e117ceb0292292cd9
hwangsanha7 <hwangsanha7@gmail.com>
2025-06-13 11:35:49 +0900

    CICD -4번부터 이상함

--- src/picar_pkg/setup.py
+++ src/picar_pkg/setup.py
@@ -1,37 +1,33 @@
-from setuptools import find_packages, setup
 import os
 from glob import glob
+from setuptools import find_packages, setup
 
 package_name = 'picar_pkg'
 
 setup(
     name=package_name,
     version='0.0.1',
-    packages=find_packages(exclude=['test']), # 'test' 폴더는 패키지에 포함 안 함
-
-    # ★★★ 이 부분이 중요합니다 ★★★
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        # launch 파일을 위한 설정 (launch 폴더를 만든다면)
-        (os.path.join('share', package_name, 'launch'), glob('launch/*launch.py')),
-        # config 파일을 위한 설정 (config 폴더가 있다면)
-        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        # launch 파일이 있다면, launch 폴더를 만들어 이 줄의 주석을 푸세요.
+        # (os.path.join('share', package_name, 'launch'), glob('launch/*launch.py')),
+        # config 파일이 있다면, 이 줄의 주석을 푸세요.
+        # (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
     ],
-
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='hsh',
-    maintainer_email='your_email@example.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-
-    # ★★★ 테스트를 위한 설정 ★★★
+    maintainer_email='your_email@example.com', # 이메일 수정 필요
+    description='Picar project ROS 2 package',
+    license='Apache License 2.0', # 라이선스에 맞게 수정
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            # 예시: '실행명 = 패키지명.파일이름:main'
+            # 예시: '실행파일이름 = 파이썬패키지경로.파일이름:main함수'
             'main_node = picar_pkg.main_node:main',
         ],
     },
+)
